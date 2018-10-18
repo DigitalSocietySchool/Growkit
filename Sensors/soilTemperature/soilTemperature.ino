9112923e14da3df7d06efee999164d823565c3d3
Joel Ruhe <>
2018-10-18 10:20:39 +0200

    soilTemperature code added

---
+++ Sensors/soilTemperature/soilTemperature.ino
@@ -0,0 +1,44 @@
+int soiltemperaturePin = A3;
+
+void setup() {
+  Serial.begin(9600);
+
+   Serial.println("Reading From the Sensor ...");
+
+   delay(2000);
+}
+
+void loop() {
+  int value = analogRead(soiltemperaturePin);
+  Serial.println(value);
+     delay(1000);
+}
+/*
+int temperaturePin = A1;
+
+void setup() {
+
+   Serial.begin(9600);
+
+   Serial.println("Reading From the Sensor ...");
+
+   delay(2000);
+
+   }
+
+void loop() {
+   
+  int value = analogRead(temperaturePin);
+
+  int adjusted_value = value * 1.024;  
+  int tempC = (adjusted_value * 0.2222) - 68.111;
+
+   Serial.print("Temperature: ");
+
+   Serial.print(tempC);
+
+   Serial.println(" C");
+
+   delay(1000);
+
+}*/
